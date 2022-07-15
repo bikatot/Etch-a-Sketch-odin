d4bbf376216dc482f8e6ca62da43387fd5964137
bikatot <vladx002@gmail.com>
2022-07-15 11:46:09 +0300

    Create the first iteration of an app

---
+++ app.js
@@ -0,0 +1,15 @@
+let a = 100;
+let b = 100;
+let grid = document.createElement("div");
+grid.setAttribute("id", "parent");
+grid.style.gridTemplateColumns = `repeat(${a}, 1fr)`;
+grid.style.gridTemplateRows = `repeat(${b}, 1fr)`;
+document.body.appendChild(grid);
+
+for (let i = 0; i < (a * b); i++) {
+  let cell = document.createElement("div");
+  cell.addEventListener("mouseover", function handleMouseOver() {
+    cell.style.backgroundColor = "black";
+  });
+  grid.appendChild(cell);
+};
